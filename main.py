fa1b9fd4ab9fea1c8bc3e7c40d0f397411894f7a
Raul <raul.turc@stud.ubbcluj.ro>
2021-09-29 08:28:49 +0300

    Rezolvarea Problemelor

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 Returneaza 1 daca n este prim si 0 daca nu.
 '''
 def is_prime(n):
-  if n > 1:
-      for i in range(2, int(n / 2) + 1):
-          if (n % i) == 0:
-              return 0
-  else:
+  if(n>1):
+    for i in range(2,int(n/2)+1):
+        if (n%i) == 0:
+          return 0
     return 1
-  return 0
+  else:
+    return 0
 
   
 '''
