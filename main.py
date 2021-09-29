929c50cfd194829c120784294a6af59cf051359a
Raul <raul.turc@stud.ubbcluj.ro>
2021-09-29 08:19:19 +0300

    Rezolvarea Problemelor

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def is_prime(n):
   if n > 1:
       for i in range(2, int(n / 2) + 1):
           if (n % i) == 0:
-              return 1
+              return 0
   else:
-    return 0
-  return 1
+    return 1
+  return 0
 
   
 '''
